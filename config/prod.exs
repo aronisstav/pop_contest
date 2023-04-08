9cf2e6d5588619a2b90e2dacd57a3a177bde5610
Stavros Aronis <stavros.aronis@protonmail.com>
2023-04-08 09:29:48 +0200

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ import Config
 
 config :pop_contest, PopContestWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://reflecting-bighearted-yardant.gigalixirapp.com"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   load_from_system_env: true,
   secret_key_base: "${SECRET_KEY_BASE}",
