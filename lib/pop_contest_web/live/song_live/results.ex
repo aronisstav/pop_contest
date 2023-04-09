d94cb88e78469be12ab392e051c12729883f102e
Stavros Aronis <stavros.aronis@protonmail.com>
2023-04-09 16:32:25 +0200

    Add total

--- lib/pop_contest_web/live/song_live/results.ex
+++ lib/pop_contest_web/live/song_live/results.ex
@@ -8,12 +8,19 @@ defmodule PopContestWeb.SongLive.Results do
   @impl true
   def mount(_params, _session, socket) do
     PopContestWeb.Endpoint.subscribe(@topic)
-    {:ok, assign(socket, :songs, Songs.list_songs())}
+    {:ok, update(socket)}
   end
 
   @impl true
   def handle_info(_msg, socket) do
-    {:noreply, assign(socket, :songs, Songs.list_songs())}
+    {:noreply, update(socket)}
   end
 
+  def update(socket) do
+    songs = Songs.list_songs()
+    total = Enum.sum(for song <- songs, do: song.tally)
+    socket
+    |> assign(:songs, songs)
+    |> assign(:total, total)
+  end
 end
