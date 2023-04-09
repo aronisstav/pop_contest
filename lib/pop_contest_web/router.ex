fa89b54a4fc7277f39f4792346e634f7dc580e6b
Stavros Aronis <stavros.aronis@protonmail.com>
2023-04-09 11:06:18 +0200

    Disable register

--- lib/pop_contest_web/router.ex
+++ lib/pop_contest_web/router.ex
@@ -52,7 +52,7 @@ defmodule PopContestWeb.Router do
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{PopContestWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
+      # live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
       live "/users/reset_password", UserForgotPasswordLive, :new
       live "/users/reset_password/:token", UserResetPasswordLive, :edit
