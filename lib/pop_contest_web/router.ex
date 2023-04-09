bca610f99d38a6e6f2a6c70da209de5f87826e44
Stavros Aronis <stavros.aronis@protonmail.com>
2023-04-09 11:05:02 +0200

    Hide songs

--- lib/pop_contest_web/router.ex
+++ lib/pop_contest_web/router.ex
@@ -21,7 +21,6 @@ defmodule PopContestWeb.Router do
     pipe_through :browser
 
     live "/", SongLive.Index, :index
-    live "/songs", SongLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
@@ -69,6 +68,7 @@ defmodule PopContestWeb.Router do
       on_mount: [{PopContestWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live "/songs", SongLive.Index, :index
       live "/songs/new", SongLive.Index, :new
       live "/songs/:id/edit", SongLive.Index, :edit
 
