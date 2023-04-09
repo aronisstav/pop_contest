c4e99475af9f8d114c7cd97895a99f14ee22d87e
Stavros Aronis <stavros.aronis@protonmail.com>
2023-04-09 11:19:43 +0200

    Fix warning

--- lib/pop_contest_web/live/song_live/vote.ex
+++ lib/pop_contest_web/live/song_live/vote.ex
@@ -13,7 +13,7 @@ defmodule PopContestWeb.SongLive.Vote do
   @impl true
   def handle_event("vote", %{"id" => id}, socket) do
     %{:tally => tally} = song = Songs.get_song!(id)
-    {:ok, newsong} = Songs.update_song(song, %{:tally => tally + 1})
+    {:ok, _} = Songs.update_song(song, %{:tally => tally + 1})
     PopContestWeb.Endpoint.broadcast_from(self(), @topic, "vote", :foo)
     {:noreply, socket}
   end
